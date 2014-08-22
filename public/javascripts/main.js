6c89e7725fba9478f3a9b6e1570da8b82523ad2c
Ryan M <ryan.megidov@staff.voo.be>
2014-08-22 17:49:28 +0200

    ADD - better remote capabilities (can actually be forced to fetch new data now) MOD - refactoring of code ADD - favicons

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,43 +1,186 @@
-/*var app = angular.module('translateApp', ['ngRoute', 'ngCookies', 'pascalprecht.translate']);
+var app = angular.module('translateApp', ['ngRoute', 'ngCookies', 'pascalprecht.translate']);
 
-window.i18n_translations = {};
+/*------------------------------------------------------------------------------
+ Ryan's little helper (aka Overwrite translation filter, cos screw the rules!)
+-------------------------------------------------------------------------------*/
+app.filter('t', ['$parse', '$translate', 
+    function ($parse, $translate) {
+        return function (translationId, interpolateParams, interpolation) {
+            if (!window.angular.isObject(interpolateParams)) interpolateParams = $parse(interpolateParams)(this);
+            return $translate.instant(translationId + '.values.' + $translate.use(), interpolateParams, interpolation);
+        };
+    }
+]);
 
-app.run(function ($http) {
-    
-    $http.get('locales/en.json', function (data) {
-        window.i18n_translations['en'] = data;
+app.run(function($rootScope, $cookies, $translate) {
+    /*----------------------------------------
+                    Initialize
+    -----------------------------------------*/
+    $rootScope.translator = {};
+    $cookies.locale = $cookies.locale || "en";
+    $translate.use($cookies.locale);
+    $rootScope.languageKeys = [];
+    $rootScope.availableLanguages = [{ key: 'en', name: 'English' }, { key: 'fr', name: 'Français' }];
+    $rootScope.availableLanguages.map(function (v) {
+        $rootScope.languageKeys.push(v.key);
     });
-    $http.get('locales/fr.json', function (data) {
-        window.i18n_translations['fr'] = data;
-    });
+
+    $rootScope.language = {
+        keys: $rootScope.languageKeys,
+        options: $rootScope.availableLanguages,
+        current: $cookies.locale
+    };
+
+});
+
+/*----------------------------------------------------------------
+ Let's write a new loader, because the original is not suitable
+-----------------------------------------------------------------*/
+app.factory('$transLoader', function ($http, $q, $timeout, $rootScope) {
+    return function(options) {
+        $rootScope.translator.selectedSet = options.selectedSet || 'Generic';
+
+        if (!options || !options.url) throw new Error('Couldn\'t use urlLoader since no url is given!');
+
+        console.debug('LOADING DATA ' + $rootScope.translator.selectedSet);
+
+        // Go fetch data from Bertrands service
+        var deferred = $q.defer();
+        $http({
+            method: 'GET',
+            url: options.url // options.url + '/' + $rootScope.translator.selectedSet
+        })
+        .success(deferred.resolve)
+        .error(function() {
+            deferred.reject(options.url);
+        });
+        
+        // Use returned json for translations
+        deferred.promise.then(function(data) {
+            $rootScope.translator.data = data; //$rootScope.$digest(); // manually propagate changes
+            //setTimeout(function(){}, 1000); //simulate network latency @TODO: Remove when deploying
+        });
+        
+        return deferred.promise;
+    };
 });
 
 app.config(function ($translateProvider) {
-    $translateProvider.translations('en', <%=getLanguage('en')%>);
-    $translateProvider.translations('fr', <%=getLanguage('fr')%>);
-    //$translateProvider.translations('fr', $http.get('javascripts/' + $scope.language.current + '.json'));
-    $translateProvider.preferredLanguage('en');
+    $translateProvider.useLoader('$transLoader', { url: 'lang' }); //more than meets the eye!
 });
 
-app.controller('langController', function ($scope, $http, $cookies, $location, $translate) {
-    
-    $scope.language = {
-        options: [{ key: 'en', name: 'English' },
-                  { key: 'fr', name: 'Français' }],
-
-        current: '<%=currentLocale%>',
-        
-        getTranslations: function () {
-                $http.get('javascripts/' + $scope.language.current + '.json', function (data) {
-                    window.i18n_translations = data;
-                });
-        },
-
-        change: function () {
-            $translate.use($scope.language.current);
-            //document.location.href = "/lang/" + $scope.language.current;
-            //$http.get('lang/' + $scope.language.current).success(function (data) { $scope.phones = data; });
+app.controller('langController', function ($scope, $http, $cookies, $location, $translate, $transLoader) {
+    /*------------------------------------------
+        Add translation set
+    -------------------------------------------*/
+    $scope.translator.add = function(trans) {
+        var index = trans.translationSets.indexOf('');
+        if (index == -1) {
+            trans.translationSets.push('');
         }
     };
 
-});*/
+    /*------------------------------------------
+        Remove translation set
+    -------------------------------------------*/
+    $scope.translator.remove = function(trans) {
+        trans.translationSets = trans.translationSets.filter(function(x) {
+            return x != '' && typeof x == 'string';
+        });
+    };
+
+    /*------------------------------------------
+        Enable/disable a field
+    -------------------------------------------*/
+    $scope.translator.enable = function(trans) {
+        trans.enabled = !trans.enabled;
+    };
+
+    /*------------------------------------------
+        Flatten multidimensional array to single
+    -------------------------------------------*/
+    $scope.translator.refresh = function() {
+        var single = [];
+        console.warn($scope.translator.data);
+        angular.forEach($scope.translator.data, function(trans, cat) {
+
+            if ($scope.translator.selectedSet != cat) return;
+
+            for (key in trans) {
+                single.push({
+                    'key': key,
+                    'values': trans[key].values,
+                    'translationSets': trans[key].translationSets
+                });
+            }
+        }, single);
+        //$translate.refresh();
+        return single;
+    };
+
+    /*------------------------------------------
+        Update a translation value
+    -------------------------------------------*/
+    $scope.translator.send = function(trans) {
+
+        /* Create the object to send */
+        var json = {};
+        json[trans.key] = {
+            'values': trans.values,
+            'translationSets': trans.translationSets
+        };
+
+        console.debug(JSON.stringify(json, null, 4));
+
+        /*$http({
+            method  : 'PUT',
+            url     : '/translations',
+            data    : json,
+            dataType: "json",
+            headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // pass info as form data (not request payload)
+        })
+        .success(function (data) {
+            $scope.infoMessage = data.message;
+            $scope.translationsTable = $scope.translator.refresh();
+            $translate.refresh();
+        })
+            .error(function (data, status) {
+            $scope.errorMessage = 'FAILED! ' + status;
+            $translate.refresh();
+        });*/
+    };
+
+    /*------------------------------------------
+       Fetch the translation set from service
+    -------------------------------------------*/
+    $scope.$watch('translator.selectedSet', function (newValue, oldValue) {
+        
+        // Ignore initial setup.
+        if (newValue == oldValue) return;
+        
+        // Reload data from service
+        $translate.refresh();
+
+        $scope.translator.selectedSet = newValue;
+        
+        //$translate.useLoader('$transLoader', { url: 'lang', selectedSet: newValue});
+        
+        // Update translation editor
+        $scope.translationsTable = $scope.translator.refresh();
+    }, true);
+    
+    $scope.$watch('language.current', function (newValue, oldValue) {
+        
+        // Ignore initial setup.
+        if (newValue == oldValue) return;
+        
+        $translate.use($scope.language.current);
+        
+        // Reload data from service
+        $translate.refresh();
+        
+        // Update translation editor
+        $scope.translationsTable = $scope.translator.refresh();
+    }, true);
+
+});
